e099f951a505feafdbe211ca51da57286da6d851
Dhaesha M <dhaesha1@gmail.com>
2022-06-15 21:47:57 -0400

    deployed to goerli test net at 0xaf5d64DD1e1056A627AD4F1b2b5a01afee0e67a8

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
-const { API_URL, PRIVATE_KEY } = process.env();
+const { API_URL, PRIVATE_KEY } = process.env;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
